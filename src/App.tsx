d2f8af582dbd89712cc916395e34c615704eac6b
Khyber Sen <kkysen@gmail.com>
2022-07-31 15:29:36 -0400

    Switched from `.js(x)` to `.ts(x)`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import * as React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;

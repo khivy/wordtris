d48813bc794d54ec56bcc639ae01749fab956c52
Emma <aith.mailbox@gmail.com>
2022-08-03 15:46:20 -0700

    Create `UserCell` class and generate `char` & `uid` for each cell

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import "./App.css";
-import { boardFromWords, createBoard, BoardStyled, BOARD_ROWS, BOARD_COLS } from "./components/Board";
+import { createBoard, BoardStyled, BOARD_ROWS } from "./components/Board";
 import { BoardCellStyled } from "./components/BoardCell";
 
 export const UserCellStyled = styled.div`
@@ -16,90 +16,112 @@ export const UserCellStyled = styled.div`
   z-index: 1;
 `;
 
-function usePlayer() {
-  // This function contains player information.
-  const [pos, setPos] = useState([2, 2] as [number, number]);
-  const [layout, setLayout] = useState([
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-  ] as const);
+class UserCell {
+    char: string;
+    uid: string;
 
-  function updatePlayerPos({ keyCode, repeat }: { keyCode: number; repeat: boolean }): void {
-    if (keyCode === 37) {
-      setPos([pos[0] - 1, pos[1]]);
-    } else if (keyCode === 39) {
-      setPos([pos[0] + 1, pos[1]]);
-    } else if (keyCode === 40) {
-      if (repeat) {
-        // TODO: Handle repeated downkey.
-      }
-      setPos([pos[0], pos[1] + 1]);
-    } else if (keyCode === 38) {
-      setPos([pos[0], pos[1] - 1]);
-    } else if (keyCode == 32) {
-      // Space bar.
-      rotateMatrix(layout);
+    constructor (char: string, uid: string) {
+        this.char = char;
+        this.uid = uid;
     }
-  }
-
-  function rotateMatrix(matrix) {
-    console.assert(matrix.length === matrix[0].length);
-    // inplace
-    let l = 0;
-    let r = matrix.length - 1;
-
-    console.log(matrix);
-    while (l < r) {
-
-      for (let i = 0; i < r-l; ++i) {
-        let bottom = r;
-        let top = l;
-        let topLeft = matrix[top][l+i];
-
-        matrix[top][l+i] = matrix[bottom-i][l];
-        matrix[bottom-i][l] = matrix[bottom][r-i];
-        matrix[bottom][r-i] = matrix[top+i][r];
-        matrix[top+i][r] = topLeft;
-      }
-      ++l;
-      --r;
-    }
-  }
-
-  function renderPlayerCell(key: string, x: number, y: number, text: string) {
-    x += pos[0];
-    y += pos[1];
-    // Center on the pivot.
-    x -= Math.floor(layout[0].length / 2);
-    y -= Math.floor(layout.length / 2);
-    // Note: the `{x,y}+1` is b.c. CSS grids' rows & cols are 1-indexed.
-    return <UserCellStyled key={key} x={x + 1} y={y + 1}>{text}</UserCellStyled>
-  }
-
-  function renderPlayerBlock() {
-    const cellsRendered = [];
-    let i = 0; // Placeholder for block characters.
-    for (let r = 0; r < BOARD_ROWS; ++r) {
-      cellsRendered.push([]);
-      for (let c = 0; c < BOARD_ROWS; ++c) {
-        if (layout[r][c] === 1) {
-          cellsRendered[r].push(renderPlayerCell("test", c, r, String.fromCharCode(97+i++)));
-        }
-      }
-    }
-    return cellsRendered;
-  }
-
-  return { updatePlayerPos, renderPlayerBlock }
 }
 
-export function App(props) {
-  const [board, setBoard] = useState(createBoard);
+function usePlayer () {
+    // This function contains player information.
+    const TBD = '@';
+    const EMPTY = '!';
+    const [pos, setPos] = useState([2, 2] as [number, number]);
+    const [matrix, setMatrix] = useState(generateCharMatrix() as const);
 
-  const { updatePlayerPos, renderPlayerBlock } = usePlayer();
+    function updatePlayerPos ({ keyCode, repeat }: { keyCode: number; repeat: boolean }): void {
+        if (keyCode === 37) {
+            setPos([pos[0] - 1, pos[1]]);
+        } else if (keyCode === 39) {
+            setPos([pos[0] + 1, pos[1]]);
+        } else if (keyCode === 40) {
+            if (repeat) {
+                // TODO: Handle repeated downkey.
+            }
+            setPos([pos[0], pos[1] + 1]);
+        } else if (keyCode === 38) {
+            setPos([pos[0], pos[1] - 1]);
+        } else if (keyCode == 32) {
+            // Space bar.
+            setMatrix(rotateMatrix(matrix));
+        }
+    }
+
+    function generateCharMatrix (): UserCell[][] {
+        // Return random starting block matrix with seed.
+        // TODO: Make it pseudo-random.
+        const res = [
+            [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, TBD, EMPTY, EMPTY],
+            [EMPTY, EMPTY, TBD, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+        ];
+        res.forEach((_, r) => res[r] = res[r].map((ch, c) => {
+            const uid = 'user(' + r + ',' + c + ')';
+            return ch === TBD ?
+                new UserCell(String.fromCharCode(Math.random(26) * 10 + 97), uid) : new UserCell(EMPTY, uid);
+        }));
+        return res;
+    }
+
+    function rotateMatrix (matrix: UserCell[][]): UserCell[][] {
+        // Inplace but returns itself.
+        console.assert(matrix.length === matrix[0].length);
+        let l = 0;
+        let r = matrix.length - 1;
+
+        while (l < r) {
+
+            for (let i = 0; i < r - l; ++i) {
+                let bottom = r;
+                let top = l;
+                let topLeft = matrix[top][l + i];
+
+                matrix[top][l + i] = matrix[bottom - i][l];
+                matrix[bottom - i][l] = matrix[bottom][r - i];
+                matrix[bottom][r - i] = matrix[top + i][r];
+                matrix[top + i][r] = topLeft;
+            }
+            ++l;
+            --r;
+        }
+        return matrix;
+    }
+
+    function renderPlayerCell (key: string, x: number, y: number, text: string) {
+        x += pos[0];
+        y += pos[1];
+        // Center on the pivot.
+        x -= Math.floor(matrix[0].length / 2);
+        y -= Math.floor(matrix.length / 2);
+        // Note: the `{x,y}+1` is b.c. CSS grids' rows & cols are 1-indexed.
+        return <UserCellStyled key={key} x={x + 1} y={y + 1}>{text}</UserCellStyled>
+    }
+
+    function renderPlayerBlock () {
+        const cellsRendered = [];
+        for (let r = 0; r < BOARD_ROWS; ++r) {
+            cellsRendered.push([]);
+            for (let c = 0; c < BOARD_ROWS; ++c) {
+                if (matrix[r][c].char != EMPTY) {
+                    cellsRendered[r].push(renderPlayerCell(matrix[r][c].uid, c, r, matrix[r][c].char));
+                }
+            }
+        }
+        return cellsRendered;
+    }
+    return { updatePlayerPos, renderPlayerBlock }
+}
+
+export function App (props) {
+    const [board, setBoard] = useState(createBoard);
+
+    const { updatePlayerPos, renderPlayerBlock } = usePlayer();
 
     useEffect(() => {
         window.addEventListener("keydown", updatePlayerPos);
@@ -109,24 +131,24 @@ export function App(props) {
         };
     });
 
-  function renderBoard() {
-    const cells = board.cells.map((row, r) => (
-      row.map((col, c) => {
-        // Note: Each component in a list should have a key.
-        // See https://reactjs.org/docs/lists-and-keys.html#keys
-        return renderCell("cell(" + r.toString() + ',' + c.toString() + ')', r + 1, c + 1);
-      })
-    ));
-    return <BoardStyled key="board" >
-        {cells}
-        {renderPlayerBlock()}
-      </BoardStyled>
-    ;
-  }
+    function renderBoard () {
+        const cells = board.cells.map((row, r) => (
+            row.map((col, c) => {
+                // Note: Each component in a list should have a key.
+                // See https://reactjs.org/docs/lists-and-keys.html#keys
+                return renderCell("cell(" + r.toString() + ',' + c.toString() + ')', r + 1, c + 1);
+            })
+        ));
+        return <BoardStyled key="board">
+            {cells}
+            {renderPlayerBlock()}
+        </BoardStyled>
+            ;
+    }
 
-  function renderCell(key: string, x: number, y: number) {
-    return <BoardCellStyled key={key} x={x} y={y} />
-  }
+    function renderCell (key: string, x: number, y: number) {
+        return <BoardCellStyled key={key} x={x} y={y}/>
+    }
 
-  return renderBoard();
+    return renderBoard();
 }

2dff18fa3b995b1fe6d6e1c788bd5f368d2db689
Emma <aith.mailbox@gmail.com>
2022-08-12 21:49:57 -0700

    Add word-matching on columns

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ class PlayerPhysics {
     layout: string[][];
     hasMoved: boolean;
 
-    constructor(board: BoardCell[][]) {
+    constructor (board: BoardCell[][]) {
         this.layout = [
             [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, TBD, EMPTY, EMPTY],
@@ -94,7 +94,7 @@ class PlayerPhysics {
         this.hasMoved = false;
     }
 
-    setPos(r: number, c: number) {
+    setPos (r: number, c: number) {
         this.pos[0] = r;
         this.pos[1] = c;
         // Update adjusted cells, which also allows React to see new updates.
@@ -103,7 +103,7 @@ class PlayerPhysics {
         );
     }
 
-    rotateCells(cells: UserCell[]): UserCell[] {
+    rotateCells (cells: UserCell[]): UserCell[] {
         console.assert(this.layout.length == this.layout[0].length);
         console.assert(this.layout.length % 2 == 1);
         let mid = Math.floor(this.layout.length / 2);
@@ -121,40 +121,40 @@ class PlayerPhysics {
         return res;
     }
 
-    getAdjustedLeftmostC() {
+    getAdjustedLeftmostC () {
         return this.adjustedCells.reduce((prev, cur) =>
             prev.c < cur.c ? prev.c : cur.c
         );
     }
 
-    getAdjustedRightmostC() {
+    getAdjustedRightmostC () {
         return this.adjustedCells.reduce((prev, cur) =>
             prev.c < cur.c ? cur.c : prev.c
         );
     }
 
-    getAdjustedTopR() {
+    getAdjustedTopR () {
         return this.adjustedCells.reduce((prev, cur) =>
             prev.r < cur.r ? prev.r : cur.r
         );
     }
 
-    getAdjustedBottomR() {
+    getAdjustedBottomR () {
         return this.adjustedCells.reduce((prev, cur) =>
             prev.r < cur.r ? cur.r : prev.r
         );
     }
 
-    isInRBounds(r: number) {
+    isInRBounds (r: number) {
         return 0 <= r && r < BOARD_ROWS;
     }
 
-    isInCBounds(c: number) {
+    isInCBounds (c: number) {
         return 0 <= c && c < BOARD_COLS;
     }
 
     // Returns the number of times crossed onto a new row.
-    doGradualFall(board: BoardCell[][]): number {
+    doGradualFall (board: BoardCell[][]): number {
         interp += interpRate;
         if (
             this.adjustedCells.some((cell) =>
@@ -174,7 +174,7 @@ class PlayerPhysics {
     }
 
     // Might be worth it to move this to GameLoop.
-    updatePlayerPos(
+    updatePlayerPos (
         board: BoardCell[][],
         { keyCode, repeat }: { keyCode: number; repeat: boolean },
     ): void {
@@ -192,7 +192,7 @@ class PlayerPhysics {
                 (!ENABLE_SMOOTH_FALL ||
                     this.isInRBounds(
                         this.getAdjustedBottomR() +
-                            Math.ceil(interp / interpMax),
+                        Math.ceil(interp / interpMax),
                     )) &&
                 areTargetSpacesEmpty(
                     Math.ceil(ENABLE_SMOOTH_FALL ? interp / interpMax : 0),
@@ -210,7 +210,7 @@ class PlayerPhysics {
                 (!ENABLE_SMOOTH_FALL ||
                     this.isInRBounds(
                         this.getAdjustedBottomR() +
-                            Math.ceil(interp / interpMax),
+                        Math.ceil(interp / interpMax),
                     )) &&
                 areTargetSpacesEmpty(
                     Math.ceil(ENABLE_SMOOTH_FALL ? interp / interpMax : 0),
@@ -304,7 +304,7 @@ class PlayerPhysics {
         }
     }
 
-    generateUserCells(): UserCell[] {
+    generateUserCells (): UserCell[] {
         // Return starting block matrix of UserCells with randomly-assigned characters.
         // TODO: Make it pseudo-random.
         let res = [];
@@ -323,7 +323,7 @@ class PlayerPhysics {
         return res;
     }
 
-    resetBlock() {
+    resetBlock () {
         this.pos = this.spawnPos.slice();
         this.cells = this.generateUserCells();
         this.setPos(this.pos[0], this.pos[1]);
@@ -333,7 +333,7 @@ class PlayerPhysics {
     }
 
     // Take a UserCell with coordinates based on the matrix, and adjust its height by `pos` and matrix center.
-    getAdjustedUserCell(cell: UserCell): UserCell {
+    getAdjustedUserCell (cell: UserCell): UserCell {
         return {
             r: cell.r + this.pos[0] - Math.floor(this.layout.length / 2),
             c: cell.c + this.pos[1] - Math.floor(this.layout[0].length / 2),
@@ -344,7 +344,7 @@ class PlayerPhysics {
 }
 
 const PlayerComponent = React.memo(
-    function PlayerComponent({ gameState, init }) {
+    function PlayerComponent ({ gameState, init }) {
         // This function contains player information.
         const playerState = useState(init); // Note: cells is not adjusted to the board.
         gameState.setPlayerCells = playerState[1];
@@ -388,11 +388,11 @@ interface BoardCell {
 class BoardPhysics {
     boardCellMatrix: BoardCell[][];
 
-    constructor(rows: number, cols: number) {
+    constructor (rows: number, cols: number) {
         this.boardCellMatrix = this.createBoard(rows, cols);
     }
 
-    resetBoard(rows, cols) {
+    resetBoard (rows, cols) {
         this.boardCellMatrix.forEach((row) =>
             row.forEach((col) => {
                 col.char = EMPTY;
@@ -400,7 +400,7 @@ class BoardPhysics {
         );
     }
 
-    createBoard(rows: number, cols: number): BoardCell[][] {
+    createBoard (rows: number, cols: number): BoardCell[][] {
         // Init cells.
         const cells = [];
         for (let r = 0; r < rows; ++r) {
@@ -413,7 +413,7 @@ class BoardPhysics {
         return cells;
     }
 
-    getGroundHeight(col: number, startRow: number): number {
+    getGroundHeight (col: number, startRow: number): number {
         // Search for first non-EMPTY board cell from the top.
         for (let row = startRow; row < BOARD_ROWS - 1; ++row) {
             if (this.boardCellMatrix[row + 1][col].char !== EMPTY) {
@@ -424,7 +424,7 @@ class BoardPhysics {
     }
 }
 
-const BoardComponent = React.memo(function BoardComponent({ gameState, init }) {
+const BoardComponent = React.memo(function BoardComponent ({ gameState, init }) {
     const boardState = useState(init);
     gameState.setBoardCells = boardState[1];
     const [board, _setBoard] = boardState;
@@ -451,7 +451,8 @@ let playerPhysics = new PlayerPhysics(boardPhysics.boardCellMatrix);
 let lockStart = null;
 // The amount of time it takes before a block locks in place.
 let lockMax = 1500;
-export function GameLoop() {
+
+export function GameLoop () {
     const gameState = {
         setPlayerCells: null,
         setBoardCells: null,
@@ -477,7 +478,8 @@ export function GameLoop() {
     let frameStep = 1000 / FPS;
     let accum = 0;
     let prevTime = performance.now();
-    function loop(timestamp) {
+
+    function loop (timestamp) {
         let curTime = performance.now();
         accum += curTime - prevTime;
         prevTime = curTime;
@@ -498,7 +500,7 @@ export function GameLoop() {
             if (
                 boardPhysics
                     .boardCellMatrix[playerPhysics.spawnPos[0]][
-                        playerPhysics.spawnPos[1]
+                    playerPhysics.spawnPos[1]
                     ].char !== EMPTY
             ) {
                 boardPhysics.resetBoard(BOARD_ROWS, BOARD_COLS);
@@ -514,15 +516,16 @@ export function GameLoop() {
         }
         window.requestAnimationFrame(loop);
     }
+
     window.requestAnimationFrame(loop);
 
-    function isPlayerTouchingGround() {
+    function isPlayerTouchingGround () {
         return playerPhysics.adjustedCells.some((cell) => {
             return cell.r >= boardPhysics.getGroundHeight(cell.c, cell.r);
         });
     }
 
-    function dropFloatingCells(): number[][] {
+    function dropFloatingCells (): number[][] {
         // Returns 2 arrays: 1 array for the coords of the floating cells, 1 array for the new coords of the floating cells.
         let added = [];
         let removed = [];
@@ -536,36 +539,36 @@ export function GameLoop() {
                     boardPhysics.boardCellMatrix[g][c].char = boardPhysics.boardCellMatrix[r][c].char;
                     boardPhysics.boardCellMatrix[r][c].char = EMPTY;
                     // Update cell in placedCells.
-                    added.push([g,c])
-                    removed.push([r,c])
+                    added.push([g, c])
+                    removed.push([r, c])
                 }
             }
         }
         return [added, removed]
     }
 
-    function findWords(arr: UserCell[], reversed: boolean): number[] {
+    function findWords (arr: UserCell[], reversed: boolean): number[] {
         // Given the array of a row or column, returns the left and right indices (inclusive) of the longest word.
-        let contents = reversed ? arr.map((cell)=> cell.char === EMPTY ? '-' : cell.char).reverse().join('') : arr.map((cell)=> cell.char === EMPTY ? '-' : cell.char).join('')
+        let contents = reversed ? arr.map((cell) => cell.char === EMPTY ? '-' : cell.char).reverse().join('') : arr.map((cell) => cell.char === EMPTY ? '-' : cell.char).join('')
         // Look for words in row
         let minWordLen = 2;
         let resLeft = -1;
         let resRight = -1;
-        for (let left=0; left<contents.length; ++left) {
-            for (let right=left+minWordLen-1; right<contents.length; ++right) {
-                let cand = contents.slice(left, right+1);
+        for (let left = 0; left < contents.length; ++left) {
+            for (let right = left + minWordLen - 1; right < contents.length; ++right) {
+                let cand = contents.slice(left, right + 1);
                 if (validWords.has(cand)) {
-                    if (right-left > resRight - resLeft) {
+                    if (right - left > resRight - resLeft) {
                         resRight = right;
                         resLeft = left;
                     }
                 }
             }
         }
-        return reversed ? [contents.length - resRight - 1, resRight - (resLeft) + (contents.length-resRight-1)] : [resLeft, resRight];
+        return reversed ? [contents.length - resRight - 1, resRight - (resLeft) + (contents.length - resRight - 1)] : [resLeft, resRight];
     }
 
-    function handleStates() {
+    function handleStates () {
         // console.log(service.state.value)
         if ("spawningBlock" == service.state.value) {
             placedCells.clear();
@@ -611,36 +614,54 @@ export function GameLoop() {
             let newBoard = boardPhysics.boardCellMatrix.slice();
             // TODO: Remove repeated checks when placedCells occupy same row or col.
             let hasRemovedWord = false;
-            for (const [r, c] of placedCells) {
-                // Row words.
-                let [left, right] = findWords(newBoard[r], false)
-                const [leftR, rightR] = findWords(boardPhysics.boardCellMatrix[r], true)
-                if (rightR - leftR > right - left) {
-                    left = leftR;
-                    right = rightR;
+            let affectedRows = new Set([...placedCells].map((cell) => cell[0]));
+            let affectedCols = new Set([...placedCells].map((cell) => cell[1]));
+            affectedRows.forEach((r) => {
+                // Row words
+                let [row_left, row_right] = findWords(newBoard[r], false);
+                const [row_leftR, row_rightR] = findWords(boardPhysics.boardCellMatrix[r], true);
+                if (row_rightR - row_leftR > row_right - row_left) {
+                    row_left = row_leftR;
+                    row_right = row_rightR;
                 }
                 // Remove word, but ignore when a candidate isn't found.
-                if (left !== -1) {
-                    console.log('removing word: ', rightR - leftR > right - left ? newBoard[r].slice(left,right+1).map((cell)=>cell.char).reverse().join('') : newBoard[r].slice(left,right+1).map((cell)=>cell.char).reverse().join(''));
-                    for (let i = left; i<right+1; ++i) {
+                if (row_left !== -1) {
+                    console.log('removing word: ', row_rightR - row_leftR > row_right - row_left ? newBoard[r].slice(row_left, row_right + 1).map((cell) => cell.char).reverse().join('') : newBoard[r].slice(row_left, row_right + 1).map((cell) => cell.char).reverse().join(''));
+                    for (let i = row_left; i < row_right + 1; ++i) {
                         newBoard[r][i].char = EMPTY;
                     }
                     hasRemovedWord = true;
                 }
+            });
+            affectedCols.forEach((c) => {
                 // Column words
-                // findWords(boardPhysics.boardCellMatrix.map((row) => row[c]), false)
-                // findWords(boardPhysics.boardCellMatrix.map((row) => row[c]), true)
-            }
+                let [col_top, col_bot] = findWords(boardPhysics.boardCellMatrix.map((row) => row[c]), false);
+                let [col_topR, col_botR] = findWords(boardPhysics.boardCellMatrix.map((row) => row[c]), true);
+                if (col_botR - col_topR > col_bot - col_top) {
+                    col_top = col_topR;
+                    col_bot = col_botR;
+                }
+                // Remove word, but ignore when a candidate isn't found.
+                if (col_top !== -1) {
+                    console.log('removing word: ', col_botR - col_topR > col_bot - col_top ? boardPhysics.boardCellMatrix.map((row) => row[c]).slice(col_top, col_bot + 1).map((cell) => cell.char).reverse().join('') : boardPhysics.boardCellMatrix.map((row) => row[c]).slice(col_top, col_bot + 1).map((cell) => cell.char).reverse().join(''));
+                    for (let i = col_top; i < col_bot + 1; ++i) {
+                        console.log('rem char', newBoard[i][c].char)
+                        newBoard[i][c].char = EMPTY;
+                    }
+                    hasRemovedWord = true;
+                }
+            });
+
             // Allow React to see changes.
             boardPhysics.boardCellMatrix = newBoard;
+            
             // Drop all characters.
             if (hasRemovedWord) {
                 const [added, _removed] = dropFloatingCells();
                 // Dropped letters can chain into more words, so stay in this state.
                 placedCells = new Set(added);
                 console.log("event: checkingMatches ~ n/a");
-            }
-            else {
+            } else {
                 service.send("DONE");
                 console.log("event: checkingMatches ~ DONE");
             }
@@ -648,5 +669,6 @@ export function GameLoop() {
         // TODO: Move this to a playerUpdate function.
         playerPhysics.hasMoved = false;
     }
+
     return res;
 }

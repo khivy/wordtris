8c5fa062b8a1fe57e357fbcdd3d99516cc540f0c
Khyber Sen <kkysen@gmail.com>
2022-08-13 21:31:52 -0400

    Formatted.

--- test/cellTest.js
+++ test/cellTest.js
@@ -1,11 +1,11 @@
-import { BoardPhysics } from '../src/App';
+import { BoardPhysics } from "../src/App";
 
 function testBaseHeight() {
     let len;
     const b = new BoardPhysics(len, len);
     for (let i = 0; i < len; ++i) {
-        console.assert(b.getGroundHeight(i, len-1) == len);
+        console.assert(b.getGroundHeight(i, len - 1) == len);
     }
 }
 
-testBaseHeight()
+testBaseHeight();

36abee323358d433c52f7e6b748c1ce2cda4ff47
Khyber Sen <kkysen@gmail.com>
2022-08-15 02:26:07 -0400

    Converted `function` func into an `=>` func when it's an argument.

--- src/BoardComponent.tsx
+++ src/BoardComponent.tsx
@@ -1,26 +1,24 @@
 import * as React from "react";
 import { BoardCellStyled } from "./components/BoardCell";
 
-export const BoardComponent = React.memo(
-    function BoardComponent({ gameState, init }) {
-        const boardState = React.useState(init);
-        gameState.setBoardCells = boardState[1];
-        const [board, _setBoard] = boardState;
+export const BoardComponent = React.memo(({ gameState, init }) => {
+    const boardState = React.useState(init);
+    gameState.setBoardCells = boardState[1];
+    const [board, _setBoard] = boardState;
 
-        const boardCells = board.map((row, r) =>
-            row.map((cell, c) => (
-                <BoardCellStyled
-                    key={`cell(${r.toString()},${c.toString()})`}
-                    r={cell.r + 1}
-                    c={cell.c + 1}
-                    char={cell.char}
-                    hasMatched={cell.hasMatched}
-                >
-                    {cell.char}
-                </BoardCellStyled>
-            ))
-        );
+    const boardCells = board.map((row, r) =>
+        row.map((cell, c) => (
+            <BoardCellStyled
+                key={`cell(${r.toString()},${c.toString()})`}
+                r={cell.r + 1}
+                c={cell.c + 1}
+                char={cell.char}
+                hasMatched={cell.hasMatched}
+            >
+                {cell.char}
+            </BoardCellStyled>
+        ))
+    );
 
-        return <>{boardCells}</>;
-    },
-);
+    return <>{boardCells}</>;
+});

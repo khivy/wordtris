e03b50694e5076f3726157f66537aec22b9608ae
Khyber Sen <kkysen@gmail.com>
2022-08-15 03:09:33 -0400

    Added a `props: BoardCell` type annotation in the `styled.dev` template string to fix more type errors.

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
+import { BoardCell } from "../BoardCell";
 import { EMPTY } from "../setup";
 
 export const BoardCellStyled = styled.div`
   width: auto;
-  background: ${(props) => {
-    if (props.char === EMPTY) {
-        return "none";
-    } else if (props.hasMatched) {
-        return "lightgreen;";
-    } else {
-        return "red";
-    }
-}};
+  background: ${(props: BoardCell) => {
+        if (props.char === EMPTY) {
+            return "none";
+        } else if (props.hasMatched) {
+            return "lightgreen;";
+        } else {
+            return "red";
+        }
+    }};
   text: ${(props) => props.char === EMPTY ? "none" : "red"};
   border: 2px solid;
   grid-row: ${(props) => props.r};

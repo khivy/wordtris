1ed7f604ee284d4cfe1b8398e9214762b0cc688a
Ivy <aith.mailbox@gmail.com>
2022-08-17 16:10:03 -0700

    Uncomment out `StrictMode`

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-    // <StrictMode> // Temporarily commented out to avoid Strict Mode's double-rendering during dev.
-    <GameLoop />,
-    // </StrictMode>,
+    <StrictMode>
+        <GameLoop/>,
+    </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function

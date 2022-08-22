8c5061e12883979087d7685d723c0d0b3c5f2098
Khyber Sen <kkysen@gmail.com>
2022-08-22 16:31:25 -0400

    `deno fmt`.

--- src/PlayerComponent.tsx
+++ src/PlayerComponent.tsx
@@ -2,34 +2,41 @@ import * as React from "react";
 import { _ENABLE_UP_KEY, ENABLE_SMOOTH_FALL, interp } from "./setup";
 import { UserCell } from "./UserCell";
 
-export const PlayerComponent = React.memo(({ isVisible, adjustedCells }: { isVisible: boolean, adjustedCells: UserCell[] }) => {
-    // This function contains player information.
+export const PlayerComponent = React.memo(
+    (
+        { isVisible, adjustedCells }: {
+            isVisible: boolean;
+            adjustedCells: UserCell[];
+        },
+    ) => {
+        // This function contains player information.
 
-    const adjustedCellsStyled = adjustedCells.map((cell) => {
-        const margin = ENABLE_SMOOTH_FALL ? interp.val : 0;
-        const divStyle = {
-            background: "lightblue",
-            border: 2,
-            borderStyle: "solid",
-            gridRow: cell.r + 1,
-            gridColumn: cell.c + 1,
-            display: "flex",
-            marginTop: `${margin}%`,
-            marginBottom: `${-margin}%`,
-            justifyContent: "center",
-            visibility: isVisible ? "visible" as const : "hidden" as const,
-            zIndex: 1,
-        };
-        return (
-            <div
-                key={cell.uid}
-                style={divStyle}
-            >
-                {cell.char}
-            </div>
-        );
-    });
+        const adjustedCellsStyled = adjustedCells.map((cell) => {
+            const margin = ENABLE_SMOOTH_FALL ? interp.val : 0;
+            const divStyle = {
+                background: "lightblue",
+                border: 2,
+                borderStyle: "solid",
+                gridRow: cell.r + 1,
+                gridColumn: cell.c + 1,
+                display: "flex",
+                marginTop: `${margin}%`,
+                marginBottom: `${-margin}%`,
+                justifyContent: "center",
+                visibility: isVisible ? "visible" as const : "hidden" as const,
+                zIndex: 1,
+            };
+            return (
+                <div
+                    key={cell.uid}
+                    style={divStyle}
+                >
+                    {cell.char}
+                </div>
+            );
+        });
 
-    // Return an array of PlayerCells, adjusted to the 1-indexed CSS Grid.
-    return <>{adjustedCellsStyled}</>;
-});
+        // Return an array of PlayerCells, adjusted to the 1-indexed CSS Grid.
+        return <>{adjustedCellsStyled}</>;
+    },
+);

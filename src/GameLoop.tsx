972080a7210a464a486c251b4993f39ad68a7d75
Ivy <aith.mailbox@gmail.com>
2022-08-30 13:51:41 -0700

    Update incorrect fetch URL

--- src/GameLoop.tsx
+++ src/GameLoop.tsx
@@ -108,7 +108,7 @@ export function GameLoop() {
     useEffect(() => {
         // Fetch validWords during countdown.
         fetch(
-            "https://raw.githubusercontent.com/khivy/wordtris/fetch_validWords/lexicons/Scrabble80K.txt",
+            "https://raw.githubusercontent.com/khivy/wordtris/main/lexicons/Scrabble80K.txt",
         )
             .then((res) => res.text())
             .then((res) => res.split("\n"))

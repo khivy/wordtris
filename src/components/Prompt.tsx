2ca278b676b3fb052483959628896c4ef74d7f1d
Ivy <aith.mailbox@gmail.com>
2022-09-10 00:46:05 -0700

    Add keyboard guide size adjustment based on screen size

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { ReactNode } from "react";
-import { BOARD_CELL_COLOR, SMALL_TEXT_SIZE } from "../setup";
+import { BOARD_CELL_COLOR } from "../setup";
+import { useEffect, useState } from "react";
+
+const SMALL_WINDOW_WIDTH_THRESHOLD = 1000;
 
 export const Prompt = React.memo(({ children, keydownCallback }: { children: ReactNode, keydownCallback: ({code}: {code: string}) => void}) => {
     // To align `<Prompt/>` above the game.
@@ -29,19 +32,36 @@ export const Prompt = React.memo(({ children, keydownCallback }: { children: Rea
 });
 
 export const Keys = React.memo(({ totalSize, keydownCallback }: { totalSize: number, keydownCallback: ({code}: {code: string}) => void }) => {
+    const [isSmallScreen, setIsSmallScreen] = useState(globalThis.innerWidth < SMALL_WINDOW_WIDTH_THRESHOLD);
+
+    function handleResize() {
+        setIsSmallScreen(globalThis.innerWidth < SMALL_WINDOW_WIDTH_THRESHOLD);
+    }
+    useEffect(() => {
+        globalThis.addEventListener("resize", handleResize);
+        return () => globalThis.removeEventListener("resize", handleResize);
+    });
+
     const keyHeight = 4;
 
     const containerStyle = {
         marginTop: `${totalSize - keyHeight}vmin`,
         display: "flex",
         justifyContent: "center",
-        gap: "6vmin",
+        gap: "3.5vmin",
         fontSize: "2.5vmin",
         whiteSpace: "break-spaces",
         color: BOARD_CELL_COLOR,
     } as const;
 
-    const guideBlockStyle = {
+    const guideCombinedBlockStyle = {
+        display: "flex",
+        flexDirection: isSmallScreen ? "column" : "row",
+        gap: "0.5vmin",
+        alignItems: "center",
+    } as const;
+
+    const guideKeyBlockStyle = {
         display: "flex",
         gap: "0.2vmin",
     } as const;
@@ -52,28 +72,36 @@ export const Keys = React.memo(({ totalSize, keydownCallback }: { totalSize: num
     } as const;
 
     const keyDivWrapper = {
-        height: `${keyHeight}vmin`,
+        display: "flex",
     } as const;
 
     return (
         <div style={containerStyle}>
-            <div style={guideBlockStyle}  >
-                <div style={keyDivWrapper} onClick={() => keydownCallback({code: "ArrowLeft"})}><Key char={"←"} keyHeight={keyHeight} /> </div>
-                <div style={keyDivWrapper} onClick={() => keydownCallback({code: "ArrowDown"})}><Key char={"↓"} keyHeight={keyHeight} /></div>
-                <div style={keyDivWrapper} onClick={() => keydownCallback({code: "ArrowRight"})}><Key char={"→"} keyHeight={keyHeight} /> </div> 
+            <div style={guideCombinedBlockStyle}>
+                <div style={guideKeyBlockStyle} >
+                    <div style={keyDivWrapper} onClick={() => keydownCallback({code: "ArrowLeft"})}><Key char={"←"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/> </div>
+                    <div style={keyDivWrapper} onClick={() => keydownCallback({code: "ArrowDown"})}><Key char={"↓"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/> </div>
+                    <div style={keyDivWrapper} onClick={() => keydownCallback({code: "ArrowRight"})}><Key char={"→"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/> </div>
+                </div>
                 <div style={guideTextStyle}>Move</div>
             </div>
-            <div style={guideBlockStyle} onClick={() => keydownCallback({code: "KeyZ"})} >
-                <Key char={"z"} keyHeight={keyHeight} />
+            <div style={guideCombinedBlockStyle}>
+                <div style={guideKeyBlockStyle} onClick={() => keydownCallback({code: "KeyZ"})}>
+                    <Key char={"z"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/>
+                </div>
                 <div style={guideTextStyle}>Rotate ↺</div>
             </div>
-            <div style={guideBlockStyle} onClick={() => keydownCallback({code: "KeyX"})}>
-                <Key char={"x"} keyHeight={keyHeight} />
-                <Key char={"↑"} keyHeight={keyHeight} />
+            <div style={guideCombinedBlockStyle}>
+                <div style={guideKeyBlockStyle} onClick={() => keydownCallback({code: "KeyX"})}>
+                    <Key char={"x"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/>
+                    <Key char={"↑"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/>
+                </div>
                 <div style={guideTextStyle}>Rotate ↻</div>
             </div>
-            <div style={guideBlockStyle} onClick={() => keydownCallback({code: "Space"})}>
-                <Key char={"space"} keyHeight={keyHeight} />
+            <div style={guideCombinedBlockStyle}>
+                <div style={guideKeyBlockStyle} onClick={() => keydownCallback({code: "Space"})}>
+                    <Key char={"space"} keyHeight={keyHeight} isSmallScreen={isSmallScreen}/>
+                </div>
                 <div style={guideTextStyle}>Drop</div>
             </div>
         </div>
@@ -81,25 +109,24 @@ export const Keys = React.memo(({ totalSize, keydownCallback }: { totalSize: num
 });
 
 export const Key = React.memo(
-    ({ char, keyHeight }: { char: string; keyHeight: number }) => {
+    ({ char, keyHeight, isSmallScreen }: { char: string; keyHeight: number, isSmallScreen: boolean }) => {
+        const adjustedKeyHeight = isSmallScreen ? keyHeight*2 : keyHeight;
+        const adjustedKeyTextHeight = isSmallScreen ? 6 : 3;
+
         const keyStyle = {
             display: "block",
-            height: `${keyHeight}vmin`,
-            width: char === "space" ? "10vmin" : "4vmin",
+            height: `${adjustedKeyHeight}vmin`,
+            width: char === "space" ? `${adjustedKeyHeight*2.5}vmin` : `${adjustedKeyHeight}vmin`,
             background: "white",
             border: `0.4vmin solid ${"grey"}`,
             borderRadius: "0.5vmin",
-            fontSize: SMALL_TEXT_SIZE,
+            fontSize: `${adjustedKeyTextHeight}vmin`,
             userSelect: "none",
             cursor: "pointer",
         } as const;
 
         const keyTextStyle = {
             textAlign: "center",
-            alignItems: "center",
-            alignContent: "center",
-            justifyItems: "center",
-            justifyContent: "center",
         } as const;
 
         return (

135532204c3609177f2480dd04cc780c6dfb0c01
Ivy <aith.mailbox@gmail.com>
2022-09-22 17:37:51 -0700

    Add placeholder MVC Controller for serving HTTP Requests

--- src/main/kotlin/khivy/wordtrisserver/WordtrisServerApplication.kt
+++ src/main/kotlin/khivy/wordtrisserver/WordtrisServerApplication.kt
@@ -1,11 +1,35 @@
 package khivy.wordtrisserver
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
-class WordtrisServerApplication
+class WordtrisServerApplication() {
+
+    class Greeting(var id: Long, val content: String) {
+    }
+
+    @RestController
+    class GreetingController {
+
+        companion object {
+            val template: String = "Hello, %s!";
+        }
+
+        var counter = AtomicLong();
+
+        @GetMapping("/greeting")
+        fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+            return Greeting(counter.incrementAndGet(), String.format(template, name));
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<WordtrisServerApplication>(*args)
+    runApplication<WordtrisServerApplication>(*args)
 }

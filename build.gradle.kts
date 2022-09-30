d98b4045206e84fbe7f7a71172bc4ea6c52c6045
Ivy <aith.mailbox@gmail.com>
2022-09-29 18:30:19 -0700

    Add Protobuf version of data

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
 	id("org.springframework.boot") version "2.7.4"
@@ -6,15 +7,18 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+	id("java")
+	id("idea")
+	id("com.google.protobuf") version "0.8.19"
+}
+
+repositories {
+	mavenCentral()
 }
 
 group = "khivy"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
-
-repositories {
-	mavenCentral()
-}
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -23,11 +27,38 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.19")
+	implementation("com.google.protobuf:protobuf-kotlin:3.21.6")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("com.google.protobuf:protobuf-java:3.21.6")
+	implementation("io.grpc:grpc-stub:1.49.1")
+	implementation("io.grpc:grpc-protobuf:1.49.1")
+	if (JavaVersion.current().isJava9Compatible()) {
+		// Workaround for @javax.annotation.Generated
+		// see: https://github.com/grpc/grpc-java/issues/3633
+		implementation("javax.annotation:javax.annotation-api:1.3.1")
+	}
+	protobuf(files("src/main/"))
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.21.6"
+	}
+
+	generatedFilesBaseDir = "$projectDir/src/main/kotlin/khivy/wordtrisserver/gen/"
+
+	generateProtoTasks {
+		ofSourceSet("main").forEach {
+			it.builtins {
+				id("kotlin")
+			}
+		}
+	}
 }
 
 tasks.withType<KotlinCompile> {

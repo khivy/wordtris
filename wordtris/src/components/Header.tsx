b617bc728aa4adec5a69f95c0d1b7105216bb0de
Ivy <aith.mailbox@gmail.com>
2022-10-09 19:28:17 -0700

    Add basic `Toggle` component for leaderboard

--- wordtris/src/components/Header.tsx
+++ wordtris/src/components/Header.tsx
@@ -1,20 +1,40 @@
 import * as React from "react";
 import { BOARD_CELL_COLOR } from "../setup";
+import { ReactNode } from "react";
 
 export const Header = React.memo(() => {
-    const style = {
+    const outerStyle = {
         zIndex: 20,
+        display: "flex",
+    } as const;
+
+    const toggleContainerStyle = {
+        height: "100vh",
+        marginLeft: "auto",
+    } as const;
+
+    const adjustTogglePositionStyle = {
+        marginTop: "3vmin",
+        marginRight: "2vmin",
     } as const;
 
     return (
-        <div style={style}>
-            <Title />
-        </div>
+        <span style={outerStyle}>
+            <div>
+                <Title/>
+            </div>
+            <div style={toggleContainerStyle}>
+                <div style={adjustTogglePositionStyle}>
+                    <Toggle title={"Leaders"}>HI</Toggle>
+                </div>
+            </div>
+        </span>
     );
 });
 
 export const Title = React.memo(() => {
     const containerStyle = {
+        background: "red",
         marginTop: "3vmin",
         marginLeft: "2vmin",
         zIndex: 20,
@@ -40,3 +60,33 @@ export const Title = React.memo(() => {
         </div>
     );
 });
+
+export const Toggle = React.memo(
+    ({ children, title }: {
+        children: ReactNode[],
+        title: string,
+    }) => {
+        const [isVisible, setIsVisible] = React.useState(false);
+
+        const staticToggleStyle = {
+            cursor: "pointer",
+        } as const;
+
+        const toggleStyle = {
+            visibility: isVisible ? "visible" as const : "hidden" as const,
+            border: "none",
+            background: "blue",
+        } as const;
+
+        return (
+            <div style={staticToggleStyle} onClick={() => {
+                setIsVisible(prev => !prev)
+            }}>
+                {title}
+                <div style={toggleStyle}>
+                    {children}
+                </div>
+            </div>
+        );
+    },
+);
